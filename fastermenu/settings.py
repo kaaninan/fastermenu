1f1147910f05831dfee1a2b34584015895459e5a
Kagan Inan <contact@kaaninan.net>
2018-06-29 19:12:11 +0300

    deploy

--- fastermenu/settings.py
+++ fastermenu/settings.py
@@ -71,8 +71,8 @@ DATABASES = {
         'NAME': 'fasterdb',
         'USER': 'fasteruser',
         'PASSWORD': 'faster(MENU)',
-        # 'HOST': 'fastermenu-db-prod.ccgtp665sryr.eu-central-1.rds.amazonaws.com',
-        'HOST': 'fastermenu-db-staging.ccgtp665sryr.eu-central-1.rds.amazonaws.com',
+        'HOST': 'fastermenu-db-prod.ccgtp665sryr.eu-central-1.rds.amazonaws.com',
+        # 'HOST': 'fastermenu-db-staging.ccgtp665sryr.eu-central-1.rds.amazonaws.com',
         'PORT': '5432',
 
         # 'NAME': os.environ['RDS_DB_NAME'],
