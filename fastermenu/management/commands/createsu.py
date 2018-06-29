5900c671a3b9ded51f732dc9ad3b9ee3961a2421
Kagan Inan <contact@kaaninan.net>
2018-06-29 22:39:55 +0300

    bugfix v2 account User has no profile

--- fastermenu/management/commands/createsu.py
+++ fastermenu/management/commands/createsu.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from account.models import *
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            uesr = User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            Profile.objects.create(user=user)
