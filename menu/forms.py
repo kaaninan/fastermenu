04b30dd8aa3ac8ff360dcb9485aba9754e96a2ac
Kagan Inan <contact@kaaninan.net>
2018-07-30 14:30:38 +0300

    fix #102

--- menu/forms.py
+++ menu/forms.py
@@ -8,8 +8,8 @@ class MenuForm(forms.ModelForm):
 	pictures = forms.ImageField(required=False, label='Ürün Resmi', help_text='Ürün resmi eklemeniz, müşterilerinizin ürünü satın alma ihtimalini arttırır. (Zorunlu değil)')
 	description = forms.CharField(max_length=200, required=False, label='Ürün Açıklaması', help_text='"Acılı sos ile servis edilir", gibi bir açıklama yazabilirsiniz.')
 	price = forms.FloatField(label='Ürün Fiyatı (TL)*', required=True)
-	stock = forms.BooleanField(label='Ürün stokta mı?', initial=True)
+	# stock = forms.BooleanField(label='Ürün stokta mı?', initial=True)
 
 	class Meta:
 		model = Menu
-		fields = ('name', 'pictures', 'description', 'price', 'stock')
+		fields = ('name', 'pictures', 'description', 'price')
