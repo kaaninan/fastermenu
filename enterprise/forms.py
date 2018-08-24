46a83d2671197690805dbb27a9c68c320a2fe26b
Kagan Inan <contact@kaaninan.net>
2018-08-24 05:02:04 +0300

    fix #111

--- enterprise/forms.py
+++ enterprise/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from PIL import Image
+import tempfile
 from .models import Enterprise
 
 
@@ -8,6 +10,33 @@ class EnterpriseForm(forms.ModelForm):
 	name = forms.CharField(max_length=30, required=False, label='Firmanınızın adı')
 	address = forms.CharField(max_length=200, required=False, label='Firmanınızın adresi', help_text='Bu kısım, sipariş ekranınıda firma isminizin altında görünür.')
 
+	logo_x = forms.IntegerField(required=False, widget = forms.HiddenInput())
+	logo_y = forms.IntegerField(required=False, widget = forms.HiddenInput())
+	logo_width = forms.IntegerField(required=False, widget = forms.HiddenInput())
+	logo_height = forms.IntegerField(required=False, widget = forms.HiddenInput())
+
 	class Meta:
 		model = Enterprise
 		fields = ('logo', 'name', 'address' )
+
+
+	def save(self):
+		form = super(EnterpriseForm, self).save()
+
+		name = self.cleaned_data.get('name')
+
+		x = self.cleaned_data.get('logo_x')
+		y = self.cleaned_data.get('logo_y')
+		w = self.cleaned_data.get('logo_width')
+		h = self.cleaned_data.get('logo_height')
+
+		image = Image.open(form.logo)
+		cropped_image = image.crop((x, y, w+x, h+y))
+		resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+		
+		tmpfile = tempfile.TemporaryFile()
+		resized_image.save(tmpfile, 'PNG', quality=100)
+
+		form.logo.save(name + '_logo.png', tmpfile)
+
+		return form
--- enterprise/models.py
+++ enterprise/models.py
@@ -8,7 +8,7 @@ class Enterprise(models.Model):
     name = models.CharField(max_length=120, verbose_name="Name")
     # perma = models.CharField(unique=True, max_length=50, verbose_name="Perma")
     address = models.CharField(max_length=200, verbose_name="Address", null=True, blank=True)
-    size = models.IntegerField(null=True, verbose_name="Size")
+    size = models.IntegerField(null=True, blank=True, verbose_name="Size")
     logo = models.ImageField(null=True, blank=True, upload_to='logos/')
     active = models.BooleanField(verbose_name="Active", default=True)
 
