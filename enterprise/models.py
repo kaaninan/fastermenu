f5e95ad1cdee5dca63d85fdace3845b58b5211bf
Kagan Inan <contact@kaaninan.net>
2018-10-16 15:19:27 +0300

    fix #162

--- enterprise/models.py
+++ enterprise/models.py
@@ -2,14 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext as _
 from barcode.models import *
 
 class Enterprise(models.Model):
+    CURRENCY = (
+        ('$', _("USD - US Dollar")),
+        ('₺', _("TRY - Turkish Lira")),
+        ('€', _("EUR - Euro")),
+        ('£', _("GBP - Pound")),
+    )
+
     name = models.CharField(max_length=120, verbose_name="Name")
     address = models.CharField(max_length=200, verbose_name="Address", null=True, blank=True)
     size = models.IntegerField(null=True, blank=True, verbose_name="Size")
     logo = models.ImageField(null=True, blank=True, upload_to='logos/')
     active = models.BooleanField(verbose_name="Active", default=True)
+    currency = models.CharField(max_length=50, verbose_name="Currency", choices=CURRENCY, default='$')
 
     def __str__(self):
         return self.name
--- menu/forms.py
+++ menu/forms.py
@@ -7,7 +7,7 @@ class MenuForm(forms.ModelForm):
 	name = forms.CharField(max_length=30, required=True, label=_("Product Name*"))
 	pictures = forms.ImageField(required=False, label=_('Picture'), help_text=_('Adding a product image increases the likelihood that your customers will buy the product. (Not required)'))
 	description = forms.CharField(max_length=200, required=False, label=_('Description'), help_text=_('You can write a description, such as "chili sauce is served with."'))
-	price = forms.FloatField(label=_('Price ($)'), required=True)
+	price = forms.FloatField(label=_('Price'), required=True)
 	# stock = forms.BooleanField(label='Ürün stokta mı?', initial=True)
 
 	class Meta:
